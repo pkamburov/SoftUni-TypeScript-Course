f37685bef6ed12faf1ad434980ae21c8c346bf4d
Pavel Kamburov <pkamburov89@gmail.com>
2025-11-07 20:35:58 +0200

    Update 05-hotelVisit.ts

--- 02-Advanced-Data-Types/05-hotelVisit.ts
+++ 02-Advanced-Data-Types/05-hotelVisit.ts
@@ -51,10 +51,7 @@ function visitFloor(floor: simplified) {
 }
 
 type simplified =
-  | ((
-      | { number: 1; train(): void }
-      | { number: 2; dine(): void }
-    ) &
+  | (({ number: 1; train(): void } | { number: 2; dine(): void }) &
       ({ hallway: "A"; pass?: "Guest" } | { hallway: "C" }))
   | { number: 3; sleep(): void; hallway: "A" | "C" };
 
@@ -79,56 +76,40 @@ visitFloor({ sleep() {}, number: 3, hallway: "A" });
 visitFloor({ dine() {}, number: 2, hallway: "C" });
 
 //Not Working
-visitFloor({ train() {}, number: 4, hallway: "A" });
-
-visitFloor({
-  train() {},
-  number: 1,
-  hallway: "C",
-  pass: "Guest",
-});
-
-visitFloor({ train() {}, number: 2, hallway: "A" });
-
-visitFloor({ train() {}, number: 3, hallway: "C" });
-
-visitFloor({
-  train() {},
-  number: 3,
-  hallway: "C",
-  pass: "Guest",
-});
-
-visitFloor({ dine() {}, number: 1, hallway: "A" });
-
-visitFloor({ dine() {}, number: 1, hallway: "B" });
-
-visitFloor({ dine() {}, number: 1, hallway: "C" });
-
-visitFloor({ dine() {}, number: 3, hallway: "C" });
-
-visitFloor({
-  dine() {},
-  number: 2,
-  hallway: "C",
-  pass: "Guest",
-});
-
-visitFloor({
-  dine() {},
-  number: 1,
-  hallway: "A",
-  pass: "Guest",
-});
-
-visitFloor({ sleep() {}, number: 3, hallway: "D" });
-
-visitFloor({ sleep() {}, number: 4, hallway: "C" });
-
-visitFloor({ sleep() {}, number: 1, hallway: "C" });
-
-visitFloor({ sleep() {}, number: 1, hallway: "A" });
-
-visitFloor({ sleep() {}, number: 2, hallway: "A" });
-
-visitFloor({ sleep() {}, number: 2, hallway: "C" });
+// visitFloor({ train() {}, number: 4, hallway: "A" });
+// visitFloor({
+//   train() {},
+//   number: 1,
+//   hallway: "C",
+//   pass: "Guest",
+// });
+// visitFloor({ train() {}, number: 2, hallway: "A" });
+// visitFloor({ train() {}, number: 3, hallway: "C" });
+// visitFloor({
+//   train() {},
+//   number: 3,
+//   hallway: "C",
+//   pass: "Guest",
+// });
+// visitFloor({ dine() {}, number: 1, hallway: "A" });
+// visitFloor({ dine() {}, number: 1, hallway: "B" });
+// visitFloor({ dine() {}, number: 1, hallway: "C" });
+// visitFloor({ dine() {}, number: 3, hallway: "C" });
+// visitFloor({
+//   dine() {},
+//   number: 2,
+//   hallway: "C",
+//   pass: "Guest",
+// });
+// visitFloor({
+//   dine() {},
+//   number: 1,
+//   hallway: "A",
+//   pass: "Guest",
+// });
+// visitFloor({ sleep() {}, number: 3, hallway: "D" });
+// visitFloor({ sleep() {}, number: 4, hallway: "C" });
+// visitFloor({ sleep() {}, number: 1, hallway: "C" });
+// visitFloor({ sleep() {}, number: 1, hallway: "A" });
+// visitFloor({ sleep() {}, number: 2, hallway: "A" });
+// visitFloor({ sleep() {}, number: 2, hallway: "C" });
