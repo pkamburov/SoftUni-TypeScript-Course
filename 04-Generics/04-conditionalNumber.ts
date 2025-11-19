1ed93ec1b0fa3d7566152f38a6dbf4a5e2910d28
Pavel Kamburov <pkamburov89@gmail.com>
2025-11-19 20:14:03 +0200

    Update 04-conditionalNumber.ts

--- 04-Generics/04-conditionalNumber.ts
+++ 04-Generics/04-conditionalNumber.ts
@@ -1,9 +1,13 @@
-function conditionalNumber<T>(arg: T extends number ? number : string) {
+type stringOrNumber<T> = T extends number ? number : string;
+
+function conditionalNumber<T>(arg: stringOrNumber<T>) {
   if (typeof arg === "number") {
     console.log(arg.toFixed(2));
   } else {
     console.log(arg);
   }
+
+  //console.log(typeof arg === 'number' ? param.toFixed(2) : param);
 }
 
 conditionalNumber<number>(20.3555);
